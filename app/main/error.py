5e96e5881bc3a6278502e050224bf314f2033581
charles okunzo <okunzopeter01@gmail.com>
2022-05-01 21:53:04 +0300

    style:add background image and link to the articles

--- app/main/error.py
+++
@@ -1,9 +0,0 @@
-# from flask import render_template
-# from . import main
-
-# @main.app_errorhandler(404)
-# def four_Ow_four():
-#   '''
-#   function to run 404 page
-#   '''
-#   return render_template('four_Ow_four.html'),404
